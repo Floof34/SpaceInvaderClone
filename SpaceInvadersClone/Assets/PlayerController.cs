68fe1ec266b462467d7d957f7b684ae324f645cb
Floof <lovro.kacic1@gmail.com>
2021-03-27 13:28:43 +0100

    Player movement
    Horizontal player movement

---
+++ SpaceInvadersClone/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float XRange = 10;
+    public float Speed = 10;
+    private float horizontalInput;
+   
+
+  
+    void Update()
+    {
+        if (transform.position.x < -XRange)
+        {
+            transform.position = new Vector3(-XRange, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.x > XRange)
+        {
+            transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * Speed);
+    }
+}
