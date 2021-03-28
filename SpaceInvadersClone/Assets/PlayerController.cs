dedf1173b310b4809101afd0d337e040f2b7fce2
Floof <lovro.kacic1@gmail.com>
2021-03-28 21:24:24 +0200

    Patch
    Slight movement fix, added props

--- SpaceInvadersClone/Assets/PlayerController.cs
+++ SpaceInvadersClone/Assets/PlayerController.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float XRange = 10;
+    public float ZRangeMax = 10;
+    public float ZRangeMin = -1;
     public float Speed = 10;
     private float horizontalInput;
-   
-
-  
+    private float horizontal1Input;
     void Update()
     {
         if (transform.position.x < -XRange)
         {
-            transform.position = new Vector3(-XRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-XRange, transform.position.y , transform.position.z);
         }
         
         if (transform.position.x > XRange)
         {
-            transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(XRange, transform.position.y , transform.position.z);
         }
-
+        if  (transform.position.z < ZRangeMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, ZRangeMin);
+        }
+        if (transform.position.z > ZRangeMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, ZRangeMax);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        horizontal1Input = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * horizontal1Input * Time.deltaTime * Speed);
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * Speed);
     }
 }
